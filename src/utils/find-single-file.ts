6306fb62d17545137950e76302c5ea1931a07b29
Abdelghani Beldjouhri <b4n92uid@gmail.com>
2025-01-01 21:42:06 -0500

    Find file by most recent mtime

--- src/utils/find-single-file.ts
+++ src/utils/find-single-file.ts
@@ -1,6 +1,27 @@
 import {glob} from 'glob'
+import {last, sortBy} from 'lodash-es'
+import {Stats} from 'node:fs'
+import {stat} from 'node:fs/promises'
 
-export async function findSingleFile(pattern: string, location?: string) {
+export async function findSingleFile(pattern: string, location?: string): Promise<string | undefined> {
   const entries = await glob(pattern, {cwd: location, absolute: true})
-  return entries.at(0)
+
+  if (entries.length === 1) {
+    return entries.at(0)
+  }
+
+  let entriesByTime = await Promise.all(
+    entries.map(async (entry): Promise<[string, Stats]> => [entry, await stat(entry)]),
+  )
+
+  entriesByTime = sortBy(entriesByTime, ([, stat]) => stat.mtimeMs)
+
+  const lastEntry = last(entriesByTime)
+
+  if (lastEntry) {
+    const [entry] = lastEntry
+    return entry
+  }
+
+  return undefined
 }
